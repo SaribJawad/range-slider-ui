09ef47cb6827271cc4f8fca19611636921eecd5e
SaribJawad <msssarib12@gmail.com>
2025-05-17 19:16:32 +0500

    add build

--- bundle.js
+++ bundle.js
@@ -1,168 +1,159 @@
-(function () { function r (e, n, t) { function o (i, f) { if (!n[i]) { if (!e[i]) { const c = typeof require === 'function' && require; if (!f && c) return c(i, !0); if (u) return u(i, !0); const a = new Error("Cannot find module '" + i + "'"); throw a.code = 'MODULE_NOT_FOUND', a } const p = n[i] = { exports: {} }; e[i][0].call(p.exports, function (r) { const n = e[i][1][r]; return o(n || r) }, p, p.exports, r, e, n, t) } return n[i].exports } for (var u = typeof require === 'function' && require, i = 0; i < t.length; i++)o(t[i]); return o } return r })()({
-  1: [function (require, module, exports) {
-    const rangeSlider = require('..')
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+const range_slider = require('../src')
 
-    const range = rangeSlider({ min: 0, max: 10 })
-    document.body.innerHTML = '<h1> range slider </h1>'
+const range = range_slider({ min: 0, max: 10 })
+document.body.innerHTML = '<h1> range slider </h1>'
 
-    const main = document.createElement('div')
-    main.classList.add('demo')
+const main = document.createElement('div')
+main.classList.add('demo')
 
-    const style = document.createElement('style')
-    style.textContent = `
-    .demo { 
-        border : 1px solid red;
-        padding : 50px;
-    }
-    `
+const style = document.createElement('style')
+style.textContent = `
+  .demo { 
+    border : 1px solid red;
+    padding : 50px;
+  }
+  `
 
-    main.append(range)
-    document.body.append(style, main)
-  }, { '..': 2 }],
-  2: [function (require, module, exports) {
-    module.exports = rangeSlider
+main.append(range)
+document.body.append(style, main)
 
-    function rangeSlider (opts) {
-      const { min = 0, max = 1000 } = opts
+},{"../src":2}],2:[function(require,module,exports){
+module.exports = range_slider
 
-      const el = document.createElement('div')
-      el.classList.add('container')
+function range_slider (opts) {
+  const { min = 0, max = 1000 } = opts
 
-      const shadow = el.attachShadow({ mode: 'closed' })
+  const el = document.createElement('div')
+  el.classList.add('container')
 
-      const input = document.createElement('input')
-      input.type = 'range'
-      input.min = min
-      input.max = max
-      input.value = min
+  const shadow = el.attachShadow({ mode: 'closed' })
 
-      input.oninput = handle_input
+  const input = document.createElement('input')
+  input.type = 'range'
+  input.min = min
+  input.max = max
+  input.value = min
 
-      const bar = document.createElement('div')
-      bar.classList.add('bar')
+  input.oninput = handle_input
 
-      const ruler = document.createElement('div')
-      ruler.classList.add('ruler')
+  const bar = document.createElement('div')
+  bar.classList.add('bar')
 
-      const fill = document.createElement('div')
-      fill.classList.add('fill')
+  const ruler = document.createElement('div')
+  ruler.classList.add('ruler')
 
-      bar.append(ruler, fill)
+  const fill = document.createElement('div')
+  fill.classList.add('fill')
 
-      const style = document.createElement('style')
-      style.textContent = get_theme()
+  bar.append(ruler, fill)
 
-      shadow.append(style, input, bar)
-      return el
+  const style = document.createElement('style')
+  style.textContent = get_theme()
 
-      // handler
-      function handle_input (e) {
-        const value = Number(e.target.value)
+  shadow.append(style, input, bar)
+  return el
 
-        fill.style.width = `${(value / max) * 100}%`
-      }
-    }
+  // handler
+  function handle_input (e) {
+    const value = Number(e.target.value)
 
-    function get_theme () {
-      return `
+    fill.style.width = `${(value / max) * 100}%`
+  }
+}
+
+function get_theme () {
+  return `
     :host {
-        box-sizing : border-box
+      box-sizing : border-box
     }
       *, *::before, *::after {
-        box-sizing: inherit;
+      box-sizing: inherit;
     }
-
     :host { 
-        --white         : hsla(0,0%,100%,1);
-        --transparent   : hsla(0,0%,0%,0);
-        --grey          :hsla(0,0%,90%,1);
-        --blue          : hsla(207,88%,66%,1);
-        position : relative;
-        width : 100%;
-        height: 16px;
+      --white         : hsla(0,0%,100%,1);
+      --transparent   : hsla(0,0%,0%,0);
+      --grey          :hsla(0,0%,90%,1);
+      --blue          : hsla(207,88%,66%,1);
+      position : relative;
+      width : 100%;
+      height: 16px;
     }
-  
     input { 
-        position : absolute;
-        top : 0;
-        left : 0;
-        width : 100%;
-        outline : none;
-        -webkit-appearance: none; 
-        margin : 0;
-        z-index : 2;
-        background-color : var(--transparent);
-        }
-
+      position : absolute;
+      top : 0;
+      left : 0;
+      width : 100%;
+      outline : none;
+      -webkit-appearance: none; 
+      margin : 0;
+      z-index : 2;
+      background-color : var(--transparent);
+    }
     .bar { 
-        position : absolute;
-        top : 3px;
-        left : 0;
-        z-index : 0;
-        height: 10px;
-        width : 100%;
-        border-radius : 8px;
-        background-color : var(--grey);
-        display : flex;
-        flex-direction : column;
-        justify-content : center;
+      position : absolute;
+      top : 3px;
+      left : 0;
+      z-index : 0;
+      height: 10px;
+      width : 100%;
+      border-radius : 8px;
+      background-color : var(--grey);
+      display : flex;
+      flex-direction : column;
+      justify-content : center;
     }
-
     .ruler {
-        position : absolute;
-        height: 6px;
-        width : 100%;
-        transform : scale(-1, 1);
-        background-size: 20px 8px;
-        background-image: repeating-linear-gradient( to right,
-         var(--grey) 0px,
-         var(--grey) 17px,
-         var(--white) 17px,
-         var(--white) 20px);
+      position : absolute;
+      height: 6px;
+      width : 100%;
+      transform : scale(-1, 1);
+      background-size: 20px 8px;
+      background-image: repeating-linear-gradient( to right,
+      var(--grey) 0px,
+      var(--grey) 17px,
+      var(--white) 17px,
+      var(--white) 20px);
     }   
-
     .fill { 
-        position : absolute;
-        height :100%;
-        width : 0%;
-        background-color : var(--grey);
+      position : absolute;
+      height :100%;
+      width : 0%;
+      background-color : var(--grey);
     }
-
     input:focus + .bar .fill,
     input:focus-within + .bar .fill,
     input:active + .bar .fill {
-        background-color : var(--blue);
+      background-color : var(--blue);
     }
 
     input::-webkit-slider-thumb {
-        -webkit-appearance: none;
-        width: 16px;
-        height: 16px;
-        border-radius : 50%;
-        background-color: var(--white);
-        border: 1px solid var(--grey);
-        cursor: pointer;
-        box-shadow: 0 3px 6px rgba(0, 0, 0, 0.4);
-        transition: background-color 0.3s, box-shadow .15s linear;
+      -webkit-appearance: none;
+      width: 16px;
+      height: 16px;
+      border-radius : 50%;
+      background-color: var(--white);
+      border: 1px solid var(--grey);
+      cursor: pointer;
+      box-shadow: 0 3px 6px rgba(0, 0, 0, 0.4);
+      transition: background-color 0.3s, box-shadow .15s linear;
     }
-
     input::-webkit-slider-thumb:hover {
-        box-shadow: 0 0 0 14px rgba(94, 176, 245, .8);
+      box-shadow: 0 0 0 14px rgba(94, 176, 245, .8);
     }
     
     input::-moz-range-thumb {
-        width: 16px;
-        height: 16px;
-        border-radius: 50%;
-        background-color: var(--white);
-        border: 1px solid var(--grey);
-        cursor: pointer;
-        transition: background-color 0.3s, box-shadow .15s linear;
+      width: 16px;
+      height: 16px;
+      border-radius: 50%;
+      background-color: var(--white);
+      border: 1px solid var(--grey);
+      cursor: pointer;
+      transition: background-color 0.3s, box-shadow .15s linear;
     }
-
     input::-moz-range-thumb:hover {
-        box-shadow: 0 0 0 14px rgba(94, 176, 245, .8);
+      box-shadow: 0 0 0 14px rgba(94, 176, 245, .8);
     }`
-    }
-  }, {}]
-}, {}, [1])
+}
+
+},{}]},{},[1]);
